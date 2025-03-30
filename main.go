d34c2762e515f43d198793c79bf97d39e9eb5ac7
Alexander Adams <icokid@gmail.com>
2025-03-30 12:23:33 -0400

    add cmd line usage and a few tweaks

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"errors"
-	"log"
+	"fmt"
 	"math/bits"
 	"math/rand/v2"
 	"os"
@@ -28,33 +28,31 @@ var (
 
 // error
 var (
-	ErrStackIsFull         = errors.New("Stack is full.")
-	ErrStackIsEmpty        = errors.New("Stack is empty.")
-	ErrNotValidInstruction = errors.New("Not a valid instruction.")
+	ErrStackIsFull         = errors.New("stack is full")
+	ErrStackIsEmpty        = errors.New("stack is empty")
+	ErrNotValidInstruction = errors.New("not a valid instruction")
 )
 
-type (
-	Game struct {
-		mem    [MaxMem]byte
-		keymap map[ebiten.Key]byte
+type Game struct {
+	mem    []byte
+	keymap map[ebiten.Key]byte
 
-		v  [MaxRegs]byte // registers
-		i  uint16        // address register
-		pc uint16        // pointer counter
-		sp uint16        // stack pointer
+	v  []byte // registers
+	i  uint16 // address register
+	pc uint16 // pointer counter
+	sp uint16 // stack pointer
 
-		stack [MaxStack]uint16
+	stack []uint16
 
-		dt byte // delay timer
-		st byte // sound timer
+	dt byte // delay timer
+	st byte // sound timer
 
-		pixels [DispWidth * DispHeight * 4]byte
-		disp   *ebiten.Image
+	pixels []byte
+	disp   *ebiten.Image
 
-		isKeyPressed bool
-		pressed      byte
-	}
-)
+	isKeyPressed bool
+	pressed      byte
+}
 
 func (g *Game) load(program string, start uint16) error {
 	data, err := os.ReadFile(program)
@@ -231,7 +229,7 @@ func (g *Game) op_8xy7(x, y byte) error {
 }
 
 // shift right v[x] by 1, store in v[x], store least-significant bit in v[f]
-func (g *Game) op_8xy6(x, y byte) error {
+func (g *Game) op_8xy6(x byte) error {
 	var least byte
 	if bits.OnesCount8(g.v[x]) > bits.OnesCount8(g.v[x]>>1) {
 		least = 1
@@ -348,8 +346,7 @@ func (g *Game) op_00ee() error {
 
 // clear display
 func (g *Game) op_00e0() error {
-	var empty [DispWidth * DispHeight * 4]byte
-	g.pixels = empty
+	g.pixels = make([]byte, DispWidth*DispHeight*4)
 	g.disp.WritePixels(g.pixels[:])
 	return nil
 }
@@ -369,7 +366,7 @@ func (g *Game) getKey() error {
 }
 
 func (g *Game) cycles(perFrame int) error {
-	for cycle := 0; cycle < perFrame; cycle++ {
+	for range perFrame {
 		opcode := g.fetch()
 		nnn := opcode & 0xfff
 		x := byte(opcode >> 8 & 0xf)
@@ -378,74 +375,109 @@ func (g *Game) cycles(perFrame int) error {
 		kk := byte(opcode & 0xff)
 		var err error
 
+		ofx65 := opcode | 0xf065
+		ofx55 := opcode | 0xf055
+		ofx33 := opcode | 0xf033
+		ofx29 := opcode | 0xf029
+		ofx1e := opcode | 0xf01e
+		ofx18 := opcode | 0xf018
+		ofx15 := opcode | 0xf015
+		ofx0a := opcode | 0xf00a
+		ofx07 := opcode | 0xf007
+		oexa1 := opcode | 0xe0a1
+		oex9e := opcode | 0xe09e
+		odxyn := opcode | 0xd000
+		ocxkk := opcode | 0xc000
+		obnnn := opcode | 0xb000
+		oannn := opcode | 0xa000
+		o9xy0 := opcode | 0x9000
+		o8xye := opcode | 0x800e
+		o8xy7 := opcode | 0x8007
+		o8xy6 := opcode | 0x8006
+		o8xy5 := opcode | 0x8005
+		o8xy4 := opcode | 0x8004
+		o8xy3 := opcode | 0x8003
+		o8xy2 := opcode | 0x8002
+		o8xy1 := opcode | 0x8001
+		o8xy0 := opcode | 0x8000
+		o7xkk := opcode | 0x7000
+		o6xkk := opcode | 0x6000
+		o5xy0 := opcode | 0x5000
+		o4xkk := opcode | 0x4000
+		o3xkk := opcode | 0x3000
+		o2nnn := opcode | 0x2000
+		o1nnn := opcode | 0x1000
+		o00ee := opcode | 0x00ee
+		o00e0 := opcode | 0x00e0
+
 		switch opcode {
-		case opcode | 0xf065:
+		case ofx65:
 			err = g.op_fx65(x)
-		case opcode | 0xf055:
+		case ofx55:
 			err = g.op_fx55(x)
-		case opcode | 0xf033:
+		case ofx33:
 			err = g.op_fx33(x)
-		case opcode | 0xf029:
+		case ofx29:
 			err = g.op_fx29(x)
-		case opcode | 0xf01e:
+		case ofx1e:
 			err = g.op_fx1e(x)
-		case opcode | 0xf018:
+		case ofx18:
 			err = g.op_fx18(x)
-		case opcode | 0xf015:
+		case ofx15:
 			err = g.op_fx15(x)
-		case opcode | 0xf00a:
+		case ofx0a:
 			err = g.op_fx0a(x)
-		case opcode | 0xf007:
+		case ofx07:
 			err = g.op_fx07(x)
-		case opcode | 0xe0a1:
+		case oexa1:
 			err = g.op_exa1(x)
-		case opcode | 0xe09e:
+		case oex9e:
 			err = g.op_ex9e(x)
-		case opcode | 0xd000:
+		case odxyn:
 			err = g.op_dxyn(x, y, n)
-		case opcode | 0xc000:
+		case ocxkk:
 			err = g.op_cxkk(x, kk)
-		case opcode | 0xb000:
+		case obnnn:
 			err = g.op_bnnn(nnn)
-		case opcode | 0xa000:
+		case oannn:
 			err = g.op_annn(nnn)
-		case opcode | 0x9000:
+		case o9xy0:
 			err = g.op_9xy0(x, y)
-		case opcode | 0x800e:
+		case o8xye:
 			err = g.op_8xye(x)
-		case opcode | 0x8007:
+		case o8xy7:
 			err = g.op_8xy7(x, y)
-		case opcode | 0x8006:
-			err = g.op_8xy6(x, y)
-		case opcode | 0x8005:
+		case o8xy6:
+			err = g.op_8xy6(x)
+		case o8xy5:
 			err = g.op_8xy5(x, y)
-		case opcode | 0x8004:
+		case o8xy4:
 			err = g.op_8xy4(x, y)
-		case opcode | 0x8003:
+		case o8xy3:
 			err = g.op_8xy3(x, y)
-		case opcode | 0x8002:
+		case o8xy2:
 			err = g.op_8xy2(x, y)
-		case opcode | 0x8001:
+		case o8xy1:
 			err = g.op_8xy1(x, y)
-		case opcode | 0x8000:
+		case o8xy0:
 			err = g.op_8xy0(x, y)
-		case opcode | 0x7000:
+		case o7xkk:
 			err = g.op_7xkk(x, kk)
-		case opcode | 0x6000:
+		case o6xkk:
 			err = g.op_6xkk(x, kk)
-		case opcode | 0x5000:
+		case o5xy0:
 			err = g.op_5xy0(x, y)
-		case opcode | 0x4000:
+		case o4xkk:
 			err = g.op_4xkk(x, kk)
-		case opcode | 0x3000:
+		case o3xkk:
 			err = g.op_3xkk(x, kk)
-		case opcode | 0x2000:
+		case o2nnn:
 			err = g.op_2nnn(nnn)
-		case opcode | 0x1000:
+		case o1nnn:
 			err = g.op_1nnn(nnn)
-		case opcode | 0x00ee:
+		case o00ee:
 			err = g.op_00ee()
-		case opcode | 0x00e0:
+		case o00e0:
 			err = g.op_00e0()
 		default:
 			err = ErrNotValidInstruction
@@ -459,7 +491,7 @@ func (g *Game) cycles(perFrame int) error {
 }
 
 func (g *Game) Update() error {
-	g.cycles(20)
+	err := g.cycles(20)
 	if g.dt > 0 {
 		g.dt--
 	} else {
@@ -472,7 +504,7 @@ func (g *Game) Update() error {
 		g.st = 60
 	}
 
-	return nil
+	return err
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
@@ -484,28 +516,52 @@ func (g *Game) Layout(outWidth, outHeight int) (int, int) {
 	return DispWidth, DispHeight
 }
 
+func NewGame(program string, start ...uint16) *Game {
+	g := &Game{
+		disp:   ebiten.NewImage(DispWidth, DispHeight),
+		dt:     60,
+		st:     60,
+		mem:    make([]byte, MaxMem),
+		v:      make([]byte, MaxRegs),
+		stack:  make([]uint16, MaxStack),
+		pixels: make([]byte, DispWidth*DispHeight*4),
+	}
+
+	mapFontset(&g.mem)
+	mapKeys(&g.keymap)
+
+	if len(start) == 0 {
+		g.load(program, DefaultStart)
+	} else if len(start) == 1 {
+		g.load(program, start[0])
+	} else {
+		fmt.Fprintf(os.Stderr, "usage: <program> [start]\n")
+		os.Exit(1)
+	}
+
+	return g
+}
+
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "usage: <program> [start]\n")
+		os.Exit(1)
+	}
+
+	game := NewGame(os.Args[1])
+
 	ebiten.SetWindowSize(DispWidth*DispScale, DispHeight*DispScale)
 	ebiten.SetWindowTitle("Chippy")
 	ebiten.SetScreenClearedEveryFrame(false)
 
-	game := &Game{
-		disp: ebiten.NewImage(DispWidth, DispHeight),
-		dt:   60,
-		st:   60,
-	}
-
-	mapFontset(&game.mem)
-	mapKeys(&game.keymap)
-	game.load("5-quirks.ch8", DefaultStart)
-
 	if err := ebiten.RunGame(game); err != nil {
-		log.Fatal(err)
+		fmt.Fprintf(os.Stderr, "%v\n", err.Error())
+		os.Exit(1)
 	}
 }
 
-func mapFontset(mem *[MaxMem]byte) {
-	fontset := [FontSetSize]byte{
+func mapFontset(mem *[]byte) {
+	fontset := []byte{
 		0xf0, 0x90, 0x90, 0x90, 0xf0, // 0
 		0x20, 0x60, 0x20, 0x20, 0x70, // 1
 		0xf0, 0x10, 0xf0, 0x80, 0xf0, // 2
@@ -523,7 +579,7 @@ func mapFontset(mem *[MaxMem]byte) {
 		0xf0, 0x80, 0xf0, 0x80, 0xf0, // E
 		0xf0, 0x80, 0xf0, 0x80, 0x80, // F
 	}
-	copy(mem[:], fontset[:])
+	copy(*mem, fontset)
 }
 
 func mapKeys(keymap *map[ebiten.Key]byte) {
